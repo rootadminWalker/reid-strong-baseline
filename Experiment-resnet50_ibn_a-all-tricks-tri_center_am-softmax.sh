0a4372c71661a32de436155fe1cde028cd73d9f0
rootadminwalker <chiioleong519@gmail.com>
2022-10-17 19:18:44 +0800

    Add AM-softmax to program

--- Experiment-resnet50_ibn_a-all-tricks-tri_center_am-softmax.sh
+++ Experiment-resnet50_ibn_a-all-tricks-tri_center_am-softmax.sh
@@ -8,5 +8,6 @@
 # last stride 1
 # bnneck on
 # with center loss
+# AM-softmax s = 30, m = 0.35
 
 python3 tools/train.py --config_file='configs/am-softmax_triplet_with_center.yml' MODEL.PRETRAIN_PATH "('pretrained_weights/r50_ibn_a.pth')" MODEL.DEVICE_ID "('1')" DATASETS.NAMES "('market1501')" MODEL.NAME  "('resnet50_ibn_a')" OUTPUT_DIR "('/home/matthew/outputs/Experiment-resnet50_ibn_a-all-tricks-tri_center_am-softmax-256x128-bs16x4-warmup10-erase0_5-labelsmooth_on-laststride1-bnneck_on-triplet_centerloss0_0005')"
