b98c4f797b8eae626aa3158c8578783c8972b48c
rootadminwalker <chiioleong519@gmail.com>
2023-02-25 16:40:56 +0800

    Added stage transform by using multiple dataloaders

--- data/build.py
+++ data/build.py
@@ -33,7 +33,6 @@ def make_data_loader(cfg):
         train_loader = DataLoader(
             train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH,
             sampler=RandomIdentitySampler(dataset.train, cfg.SOLVER.IMS_PER_BATCH, cfg.DATALOADER.NUM_INSTANCE),
-            # sampler=RandomIdentitySampler_alignedreid(dataset.train, cfg.DATALOADER.NUM_INSTANCE),    # new add by gu
             num_workers=num_workers, collate_fn=train_collate_fn
         )
 
@@ -43,3 +42,47 @@ def make_data_loader(cfg):
         collate_fn=val_collate_fn
     )
     return train_loader, val_loader, len(dataset.query), num_classes
+
+
+def make_data_loaders_with_stages(cfg):
+    assert len(cfg.SOLVER.STAGE_TRANSFORMS) == len(cfg.SOLVER.STAGE_PERIOD), \
+        f"STAGE_PERIOD and STATE_TRANSFORMS must have the same length, " \
+        f"but got {cfg.SOLVER.STAGE_TRANSFORMS} and {cfg.SOLVER.STAGE_PERIOD} "
+    train_loaders = []
+
+    use_rgb = cfg.DATALOADER.USE_RGB
+    dataset = init_dataset(cfg.DATASETS.NAMES, root=cfg.DATASETS.ROOT_DIR, aug_per_image=cfg.SOLVER.AUG_PER_IMG)
+    num_workers = cfg.DATALOADER.NUM_WORKERS
+    num_classes = dataset.num_train_pids
+
+    val_transforms = build_transforms_stage(cfg, is_train=False)
+    val_set = ImageDataset(dataset.query + dataset.gallery, use_rgb, transform=val_transforms)
+    val_loader = DataLoader(
+        val_set, batch_size=cfg.TEST.IMS_PER_BATCH, shuffle=False, num_workers=num_workers,
+        collate_fn=val_collate_fn
+    )
+
+    for st_idx in range(len(cfg.SOLVER.STAGE_TRANSFORMS) + 1):
+        stage_transforms = cfg.SOLVER.STAGE_TRANSFORMS[:st_idx]
+        train_transforms = build_transforms_stage(cfg, stage_transforms=stage_transforms, is_train=True)
+        train_set = ImageDataset(dataset.train, use_rgb, transform=train_transforms)
+        if cfg.DATALOADER.SAMPLER == 'softmax':
+            train_loader = DataLoader(
+                train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH, shuffle=True, num_workers=num_workers,
+                collate_fn=train_collate_fn
+            )
+        else:
+            train_loader = DataLoader(
+                train_set, batch_size=cfg.SOLVER.IMS_PER_BATCH,
+                sampler=RandomIdentitySampler(dataset.train, cfg.SOLVER.IMS_PER_BATCH, cfg.DATALOADER.NUM_INSTANCE,
+                                              fixed_epoch_steps=cfg.SOLVER.EVAL_INTERVAL),
+                num_workers=num_workers, collate_fn=train_collate_fn
+            )
+
+        train_loaders.append(train_loader)
+
+    return train_loaders, val_loader, len(dataset.query), num_classes
+
+
+def make_pl_datamodule(cfg):
+    return REIDDataModule(*make_data_loaders_with_stages(cfg), cfg.SOLVER.STAGE_PERIOD)
--- data/transforms/build.py
+++ data/transforms/build.py
@@ -8,6 +8,10 @@ import torchvision.transforms as T
 
 from .transforms import RandomErasing
 
+name_to_inst = {
+    "AugMix": T.AugMix()
+}
+
 
 def build_transforms(cfg, is_train=True):
     extra_augs = []
@@ -33,4 +37,34 @@ def build_transforms(cfg, is_train=True):
             normalize_transform
         ])
 
+    return transform
+
+
+def build_transforms_stage(cfg, stage_transforms=None, is_train=True):
+    if stage_transforms is None:
+        stage_transforms = []
+
+    normalize_transform = T.Normalize(mean=cfg.INPUT.PIXEL_MEAN, std=cfg.INPUT.PIXEL_STD)
+    if is_train:
+        insert_transforms = []
+        for stage_transform in stage_transforms:
+            insert_transforms.append(name_to_inst[stage_transform])
+        transform = T.Compose([
+            T.Resize(cfg.INPUT.SIZE_TRAIN),
+            *insert_transforms,
+            T.RandomHorizontalFlip(p=cfg.INPUT.PROB),
+            T.Pad(cfg.INPUT.PADDING),
+            T.RandomCrop(cfg.INPUT.SIZE_TRAIN),
+            T.ToTensor(),
+            normalize_transform,
+            RandomErasing(probability=cfg.INPUT.RE_PROB, mean=cfg.INPUT.PIXEL_MEAN)
+        ])
+    else:
+        transform = T.Compose([
+            T.ToTensor(),
+            T.Resize(cfg.INPUT.SIZE_TEST),
+            normalize_transform
+        ])
+
+    print(f'Transforms: {transform}')
     return transform
