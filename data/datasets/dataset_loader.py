c256b29de40839a5af63fafb78a2bf8b789fc593
rootadminwalker <chiioleong519@gmail.com>
2023-02-25 16:36:29 +0800

    Added flag that can select whether using RGB or BGR to read the image

--- data/datasets/dataset_loader.py
+++ data/datasets/dataset_loader.py
@@ -5,13 +5,13 @@
 """
 
 import os.path as osp
-import warnings
 
 from PIL import Image
 from torch.utils.data import Dataset
+import cv2 as cv
 
 
-def read_image(img_path):
+def read_image(img_path, use_rgb):
     """Keep reading image until succeed.
     This can avoid IOError incurred by heavy IO process."""
     got_img = False
@@ -19,7 +19,12 @@ def read_image(img_path):
         raise IOError("{} does not exist".format(img_path))
     while not got_img:
         try:
-            img = Image.open(img_path).convert('RGB')
+            # img = Image.open(img_path).convert('RGB')
+            img = cv.imread(img_path)
+            if use_rgb:
+                img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+            img = Image.fromarray(img)
             got_img = True
         except IOError:
             print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
@@ -30,8 +35,9 @@ def read_image(img_path):
 class ImageDataset(Dataset):
     """Image Person ReID Dataset"""
 
-    def __init__(self, dataset, transform=None):
+    def __init__(self, dataset, use_rgb, transform=None):
         self.dataset = dataset
+        self.use_rgb = use_rgb
         self.transform = transform
 
     def __len__(self):
@@ -39,7 +45,7 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, index):
         img_path, pid, camid = self.dataset[index]
-        img = read_image(img_path)
+        img = read_image(img_path, self.use_rgb)
 
         if self.transform is not None:
             img = self.transform(img)
---
+++ tools/train_lightning.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""
+@original_author:  sherlock
+@contact: sherlockliao01@gmail.com
+@pytorch_lightning_revise: rootadminWalker
+"""
+
+import os
+import sys
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+
+sys.path.append('.')
+from data import make_pl_datamodule
+from engine.reid_module import PersonReidModule
+from solver.build import build_warmup_lr
+
+from utils import setup_cli, setup_loggers
+
+
+def train(cfg):
+    output_dir = cfg.OUTPUT_DIR
+    tb_logs_path = cfg.TB_LOG_DIR
+
+    # prepare dataset
+    datamodule = make_pl_datamodule(cfg)
+
+    # Setup callbacks
+    warmup_lr = build_warmup_lr(cfg)
+    lr_monitor = LearningRateMonitor(
+        logging_interval='step'
+    )
+    epoch_checkpoint_cb = ModelCheckpoint(
+        dirpath=output_dir,
+        every_n_epochs=cfg.SOLVER.CHECKPOINT_PERIOD,
+        save_top_k=-1
+    )
+    top_mAP_checkpoint_cb = ModelCheckpoint(
+        dirpath=output_dir,
+        save_top_k=1,
+        monitor='mAP',
+        mode='max',
+        filename=cfg.MODEL.NAME + "_best-mAP-{epoch:2d}-{mAP}"
+    )
+    top_rank1_checkpoint_cb = ModelCheckpoint(
+        dirpath=output_dir,
+        save_top_k=1,
+        monitor='rank1',
+        mode='max',
+        filename=cfg.MODEL.NAME + "_best-rank1-{epoch:2d}-{rank1}"
+    )
+
+    logger = TensorBoardLogger(
+        os.path.join(output_dir, tb_logs_path),
+        name="reid-train"
+    )
+
+    # No center not supported now
+    assert cfg.MODEL.IF_WITH_CENTER == 'yes', "Not supported for no center"
+
+    # Add for using self trained model
+    assert cfg.MODEL.PRETRAIN_CHOICE in ['self', 'imagenet'], \
+        'Only support pretrain_choice for imagenet and self, but got {}'.format(cfg.MODEL.PRETRAIN_CHOICE)
+    checkpoint_path = None
+    if cfg.MODEL.PRETRAIN_CHOICE == 'self':
+        checkpoint_path = cfg.MODEL.PRETRAIN_PATH
+
+    module = PersonReidModule(cfg, num_classes=datamodule.num_classes, num_queries=datamodule.num_queries)
+    trainer = pl.Trainer(
+        accelerator=cfg.MODEL.DEVICE,
+        devices=list(map(int, cfg.MODEL.DEVICE_ID)),
+        benchmark=True,
+        logger=logger,
+        callbacks=[
+            warmup_lr,
+            lr_monitor,
+            epoch_checkpoint_cb,
+            top_mAP_checkpoint_cb,
+            top_rank1_checkpoint_cb
+        ],
+        max_epochs=cfg.SOLVER.MAX_EPOCHS,
+        # check_val_every_n_epoch=cfg.SOLVER.EVAL_PERIOD,
+        val_check_interval=cfg.SOLVER.EVAL_INTERVAL - 1,
+        num_sanity_val_steps=0
+    )
+    trainer.fit(module, datamodule=datamodule, ckpt_path=checkpoint_path)
+
+
+def main():
+    cfg, args = setup_cli()
+    logger = setup_loggers(args, cfg.OUTPUT_DIR)
+    train(cfg)
+
+
+if __name__ == '__main__':
+    main()
