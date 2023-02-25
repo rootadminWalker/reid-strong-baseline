ad8b9454ac7dccc55f75f0d275023f1391ab7144
rootadminwalker <chiioleong519@gmail.com>
2023-02-25 17:03:08 +0800

    Bash script for training

---
+++ Train-AugMix.bash
@@ -0,0 +1,1 @@
+python3 tools/train_lightning.py --config_file=configs/AugMix.yml MODEL.NAME "resnet50_ibn_a" MODEL.DEVICE_ID "'0'" DATASETS.ROOT_DIR "/media/root_walker/DATA/datasets" DATASETS.NAMES "('market1501')" OUTPUT_DIR "/media/root_walker/DATA/outputs/Market1501_outputs/2023_2_24/AugMix-warmupiters-5"
